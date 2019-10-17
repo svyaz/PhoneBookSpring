62182ec4f06229db4386d22be72715e26b0f0cdc
Max Kuzhelev <maxim.kuzhelev@gmail.com>
2019-10-18 00:09:14 +0700

    Логирование в ErrorController

--- src/main/java/ru/academits/phonebook/ErrorController.java
+++ src/main/java/ru/academits/phonebook/ErrorController.java
@@ -1,5 +1,7 @@
 package ru.academits.phonebook;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -7,9 +9,12 @@ import ru.academits.model.ErrorInfo;
 
 @ControllerAdvice
 public class ErrorController {
+    private static final Logger logger = LoggerFactory.getLogger(ErrorController.class);
+
     @ExceptionHandler(Exception.class)
     @ResponseBody
     public ErrorInfo processException(Exception e) {
+        logger.error("processException: " + e.getMessage());
         return new ErrorInfo(e.getMessage());
     }
 }
