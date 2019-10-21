4d9018033f5dcabb01cee046aa79f3f01bca96f2
Max Kuzhelev <maxim.kuzhelev@gmail.com>
2019-10-22 00:10:36 +0700

    Тест получения контактов через всё приложение с поднятием Spring

--- src/test/java/ru/academits/PhonebookSpringApplicationTests.java
+++ src/test/java/ru/academits/PhonebookSpringApplicationTests.java
@@ -1,14 +1,27 @@
 package ru.academits;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import ru.academits.model.Contact;
+import ru.academits.phonebook.PhoneBookController;
+
+import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class PhonebookSpringApplicationTests {
-	@Test
-	public void contextLoads() {
-	}
+    @Autowired
+    private PhoneBookController phoneBookController;
+
+    @Test
+    public void getAllContactsTest() {
+        List<Contact> contactList = phoneBookController.getAllContacts();
+        Assert.assertEquals(contactList.size(), 2);
+        Assert.assertEquals(contactList.get(0).getPhone(), "9123456789");
+        Assert.assertEquals(contactList.get(1).getPhone(), "9131234567");
+    }
 }
